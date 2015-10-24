771bc8dea3f7c8d907b5b50072b19801f303afe2
Cameron Wheeler <cameron@scelos.com>
2015-10-24 02:11:08 -0400

    Redis auth working

--- server.js
+++ server.js
@@ -3,11 +3,12 @@ var bodyParser = require('body-parser');
 var cookieParser = require('cookie-parser');
 var session = require('express-session');
 var Passport = require('passport');
-var Redis = require("redis");
-
+var Redis = require("redis-node");
+var LocalStrategy = require('passport-local').Strategy;
+var Bcrypt = require('bcrypt');
 var app = express();
 
-var rclient = Redis.createClient();
+var rclient = Redis.createClient(); 
 
 app.use(bodyParser.urlencoded({
   extended: true
@@ -22,6 +23,37 @@ app.use(session({
 
 app.use(Passport.initialize());
 app.use(Passport.session());
+
+Passport.use(new LocalStrategy(
+  function(username, password, done) {
+    rclient.exists(username, function(err, exists) {
+    	if (exists) {
+	    	rclient.hgetall(username, function (err, resp) {
+	    		console.log(resp);
+	    		console.log(password);
+	    		if (Bcrypt.compareSync(password, resp.pass)) {
+					return done(null, resp);
+	    		} else {
+	    			return done(null, false, { message: 'Wrong password.' });
+	    		}
+		      	
+		    });
+    	}
+    });
+  }
+));
+
+Passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+
+Passport.deserializeUser(function(user, done) {
+  done(null, user)
+});
+
+app.get('/login', Passport.authenticate('local'), function(req, res) {
+    res.send("valid_auth");
+});
 
 app.use(express.static(__dirname + '/app'));
 app.listen(3000);
