41ae59e77ddc6b1fb5ca0b22a2ec88492a7d60ea
Cameron Wheeler <cameron@scelos.com>
2015-10-24 06:40:01 -0400

    Started modal for registering device

--- app/controllers/dashboardController.js
+++ app/controllers/dashboardController.js
@@ -1,5 +1,9 @@
 familee.controller('dashboardController', ['$scope', '$timeout', '$location', '$http', function($scope, $timeout, $location, $http) {
 	$scope.fullname = "";
+	$scope.showRelativeNext = false;
+	$scope.addRelative2 = false;
+	$scope.addRelative1 = true;
+	$scope.addingDevice = "";
 
 	$http({
 	    method  : 'GET',
@@ -9,4 +13,12 @@ familee.controller('dashboardController', ['$scope', '$timeout', '$location', '$
 	    var resItems = angular.fromJson(resp);
 	    $scope.fullname = resItems.name;
 	});
+
+	$scope.chooseDevice = function (device) {
+		$scope.addRelative1 = false;
+		$scope.addRelative2 = true;
+
+		$scope.addingDevice = device;
+		$scope.showRelativeNext = true;
+	};
 }]);
