a6b2ca9976e55d18a28ef5311b760ad22d487f4a
Cameron Wheeler <cameron@scelos.com>
2015-10-24 05:35:46 -0400

    More dashboard fixes

--- app/controllers/dashboardController.js
+++ app/controllers/dashboardController.js
@@ -1,2 +1,12 @@
-familee.controller('dashboardController', ['$scope', '$timeout', '$location', function($scope, $timeout, $location) {
+familee.controller('dashboardController', ['$scope', '$timeout', '$location', '$http', function($scope, $timeout, $location, $http) {
+	$scope.fullname = "";
+
+	$http({
+	    method  : 'GET',
+	    url     : '/userinfo',
+	})
+	.success(function(resp) {
+	    var resItems = angular.fromJson(resp);
+	    $scope.fullname = resItems.name;
+	});
 }]);
