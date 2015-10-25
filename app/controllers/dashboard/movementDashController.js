cb6353f451873375bdb859a7ef98ac78c38c0a1d
Cameron Wheeler <cameron@scelos.com>
2015-10-25 02:15:56 -0400

    Sample data to avoid rate limiting :(

--- app/controllers/dashboard/movementDashController.js
+++ app/controllers/dashboard/movementDashController.js
@@ -2,15 +2,7 @@ familee.controller('movementDashController', ['$scope', '$timeout', '$location',
 	$scope.labels = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
 	$scope.series = ['Steps Per Day'];
 
-	console.log($rootScope.steps['activities-tracker-steps']);
-
 	$scope.data = [[65, 59, 80, 81, 56, 55, 40]];
-
-	for (var i = 0; i < $rootScope.steps.length; i++) {
-		//$rootScope.steeps[i]
-	};
-
-	console.log($rootScope.steps);
 
 	$scope.labels = [];
 	$scope.series = ['Steps Per Day'];
