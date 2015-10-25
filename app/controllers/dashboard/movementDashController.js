11042e307011116674ccd11cbb99aaa8ae6ac416
Cameron Wheeler <cameron@scelos.com>
2015-10-25 02:36:04 -0400

    Movement stuff

--- app/controllers/dashboard/movementDashController.js
+++ app/controllers/dashboard/movementDashController.js
@@ -1,6 +1,8 @@
 familee.controller('movementDashController', ['$scope', '$timeout', '$location', '$http', '$rootScope', function($scope, $timeout, $location, $http, $rootScope) {
 	$scope.labels = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
 	$scope.series = ['Steps Per Day'];
+	$scope.numSteps = 0;
+	$scope.level = "";
 
 	$scope.data = [[65, 59, 80, 81, 56, 55, 40]];
 
@@ -13,5 +15,14 @@ familee.controller('movementDashController', ['$scope', '$timeout', '$location',
 		console.log($rootScope.steps['activities-tracker-steps'][i]);
 		$scope.data[0].push($rootScope.steps['activities-tracker-steps'][i].value);
 		$scope.labels.push($rootScope.steps['activities-tracker-steps'][i].dateTime);
+		$scope.numSteps += parseInt($rootScope.steps['activities-tracker-steps'][i].value);
 	};
+
+	$scope.numSteps = parseInt($scope.numSteps/30, 10);
+
+	if ($scope.numSteps < 2000) {
+		$scope.level = "Below Average";
+	} else {
+		$scope.level = "Above Average";
+	}
 }]);
