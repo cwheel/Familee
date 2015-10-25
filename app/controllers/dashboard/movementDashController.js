e4c3f88230104617b732b98578d42f7054f11202
Cameron Wheeler <cameron@scelos.com>
2015-10-25 01:12:57 -0400

    Movement graphs done

--- app/controllers/dashboard/movementDashController.js
+++ app/controllers/dashboard/movementDashController.js
@@ -2,6 +2,8 @@ familee.controller('movementDashController', ['$scope', '$timeout', '$location',
 	$scope.labels = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
 	$scope.series = ['Steps Per Day'];
 
+	console.log($rootScope.steps['activities-tracker-steps']);
+
 	$scope.data = [[65, 59, 80, 81, 56, 55, 40]];
 
 	for (var i = 0; i < $rootScope.steps.length; i++) {
@@ -9,4 +11,15 @@ familee.controller('movementDashController', ['$scope', '$timeout', '$location',
 	};
 
 	console.log($rootScope.steps);
+
+	$scope.labels = [];
+	$scope.series = ['Steps Per Day'];
+
+	$scope.data = [[]];
+
+	for (var i = 0; i < $rootScope.steps['activities-tracker-steps'].length; i++) {
+		console.log($rootScope.steps['activities-tracker-steps'][i]);
+		$scope.data[0].push($rootScope.steps['activities-tracker-steps'][i].value);
+		$scope.labels.push($rootScope.steps['activities-tracker-steps'][i].dateTime);
+	};
 }]);
--- app/controllers/dashboardController.js
+++ app/controllers/dashboardController.js
@@ -39,6 +39,73 @@ familee.controller('dashboardController', ['$scope', '$timeout', '$state', '$htt
 	    	$rootScope.selectedRow = $scope.devices[0].owner;
 		}
 
+		$scope.update();
+	});
+
+	$http({
+	    method  : 'GET',
+	    url     : '/userinfo',
+	})
+	.success(function(resp) {
+	    var resItems = angular.fromJson(resp);
+	    $scope.fullname = resItems.name;
+	});
+
+	$scope.nextAddReminder =  function() {
+		if ($scope.addReminder2 == false) {
+			$scope.addReminder2 = true;
+			$scope.addReminder1 = false;
+		} else {
+			$http({
+			    method  : 'POST',
+			    url     : '/addReminder',
+			    data    : $.param({phone : $scope.newReminder.phone, mssg : $scope.newReminder.mssg, block : $scope.newReminder.block}),
+			    headers : { 'Content-Type': 'application/x-www-form-urlencoded' }
+			})
+			.success(function(resp) {
+			    if (resp == "valid_auth") {
+			      	$state.go("dashboard");
+			    }
+			})
+			$("#addReminder").modal('hide');
+		}
+	};
+
+	$scope.chooseDevice = function (device) {
+		$scope.addRelative1 = false;
+		$scope.addRelative2 = true;
+
+		$scope.addingDevice = device;
+		$scope.showRelativeNext = true;
+
+	};
+
+	$scope.selectRow = function(row) {
+		$rootScope.selectedRow = row;
+		$scope.update();
+	};
+
+	$scope.next = function() {
+		//So, so truely terrible...
+		//Yet so, so easy to do...
+		if ($("#secondaryAddTitle").text() == "Setup Complete!") {
+			$("#addRelative").modal('hide');
+
+			$setTimeout(function() {
+				$scope.showRelativeNext = false;
+				$scope.addRelative2 = false;
+				$scope.addRelative1 = true;
+
+				$("#secondaryAddTitle").text("Connect to Fitbit");
+				$("#secondaryAddMessage").text("Great, to set up your relatives Fitbit {{ addingDevice }} we'll need to have you login with the Fitbit account the device is associated with. This account may be registered to you, or to your relative depending on who's device it actually is. When your ready, press Next and we'll open up Fitbits website for you to login in at.");
+				$("#nextButton").text("Next");
+			}, 1000);
+		} else {
+			window.open("http://localhost:3000/connect/fitbit2")
+		}
+	};
+
+	$scope.update = function() {
 		/////////////////////////////////
 		//All the data from the server//
 		////////////////////////////////
@@ -90,68 +157,5 @@ familee.controller('dashboardController', ['$scope', '$timeout', '$state', '$htt
 		////////
 		//End//
 		///////
-	});
-
-	$http({
-	    method  : 'GET',
-	    url     : '/userinfo',
-	})
-	.success(function(resp) {
-	    var resItems = angular.fromJson(resp);
-	    $scope.fullname = resItems.name;
-	});
-
-	$scope.nextAddReminder =  function() {
-		if ($scope.addReminder2 == false) {
-			$scope.addReminder2 = true;
-			$scope.addReminder1 = false;
-		} else {
-			$http({
-			    method  : 'POST',
-			    url     : '/addReminder',
-			    data    : $.param({phone : $scope.newReminder.phone, mssg : $scope.newReminder.mssg, block : $scope.newReminder.block}),
-			    headers : { 'Content-Type': 'application/x-www-form-urlencoded' }
-			})
-			.success(function(resp) {
-			    if (resp == "valid_auth") {
-			      	$state.go("dashboard");
-			    }
-			})
-			$("#addReminder").modal('hide');
-		}
-	};
-
-	$scope.chooseDevice = function (device) {
-		$scope.addRelative1 = false;
-		$scope.addRelative2 = true;
-
-		$scope.addingDevice = device;
-		$scope.showRelativeNext = true;
-
-	};
-
-	$scope.selectRow = function(row) {
-		$rootScope.selectedRow = row;
-	};
-
-	$scope.next = function() {
-		//So, so truely terrible...
-		//Yet so, so easy to do...
-		if ($("#secondaryAddTitle").text() == "Setup Complete!") {
-			$("#addRelative").modal('hide');
-
-			$setTimeout(function() {
-				$scope.showRelativeNext = false;
-				$scope.addRelative2 = false;
-				$scope.addRelative1 = true;
-
-				$("#secondaryAddTitle").text("Connect to Fitbit");
-				$("#secondaryAddMessage").text("Great, to set up your relatives Fitbit {{ addingDevice }} we'll need to have you login with the Fitbit account the device is associated with. This account may be registered to you, or to your relative depending on who's device it actually is. When your ready, press Next and we'll open up Fitbits website for you to login in at.");
-				$("#nextButton").text("Next");
-			}, 1000);
-		} else {
-			window.open("http://localhost:3000/connect/fitbit2")
-		}
-		
 	};
 }]);
